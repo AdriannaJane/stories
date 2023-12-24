1331683559adb36a2896a1bc1b6eeb93fe665ffa
AdriannaJane <adrianna.jane@outlook.com>
2023-12-24 02:03:20 -0500

    updates for mobile fix

--- script.js
+++ script.js
@@ -148,7 +148,6 @@
 //     }
 // });
 
-
 document.addEventListener('DOMContentLoaded', function () {
     const drawCardButton = document.getElementById('draw-card-button');
     const topCard = document.getElementById('top-card');
@@ -156,31 +155,32 @@ document.addEventListener('DOMContentLoaded', function () {
     let seenPrompts = [];
     let prompts = []; // Array to store your prompts
     let isFirstLoad = true; // Flag to track the first interaction
-    let isDragging = false;
-    let startX, startY, endX, endY;
 
     // Load initial prompts
     loadPrompts();
 
-    // Function to flip the card to show the back
-    function flipCardToShowBack() {
-        if (!topCard.classList.contains('flipped')) {
-            topCard.classList.add('flipped');
-        }
+    // Function to flip the card
+    function flipCard() {
+        topCard.classList.toggle('flipped');
     }
 
     // Function to draw a new card
     function drawNewCard() {
         if (isFirstLoad) {
-            flipCardToShowBack(); // Only flip the card on the first load
+            // Only flip the card on the first load
+            flipCard();
             isFirstLoad = false;
         } else {
-            flipCardToShowBack();
-            setTimeout(() => {
-                flyAway(1000, 0); // Fly off-screen after flipping
-            }, 600); // Delay to allow flip animation to complete
+            // Load next prompt without flipping if card is already flipped
+            if (topCard.classList.contains('flipped')) {
+                loadNextPrompt();
+            } else {
+                flipCard();
+                setTimeout(loadNextPrompt, 600); // Delay to load next prompt after flipping
+            }
         }
     }
+
     topCard.addEventListener('click', drawNewCard);
     drawCardButton.addEventListener('click', drawNewCard);
 
@@ -204,6 +204,13 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     }
 
+    function loadNextPrompt() {
+        loadNewPrompt(promptElement);
+        if (!topCard.classList.contains('flipped')) {
+            flipCard();
+        }
+    }
+
     function getNextPrompt() {
         const availablePrompts = prompts.filter(prompt => !seenPrompts.includes(prompt));
         if (availablePrompts.length === 0) {
@@ -214,98 +221,5 @@ document.addEventListener('DOMContentLoaded', function () {
         const randomPrompt = availablePrompts[randomIndex];
         seenPrompts.push(randomPrompt);
         return randomPrompt;
-    }
-
-        // Drag functionality for the top card
-    // topCard.addEventListener('mousedown', startDrag);
-    // topCard.addEventListener('touchstart', startDrag);
-
-    // function startDrag(e) {
-    //     isDragging = true;
-    //     startX = e.type.includes('mouse') ? e.pageX : e.touches[0].pageX;
-    //     startY = e.type.includes('mouse') ? e.pageY : e.touches[0].pageY;
-    //     topCard.classList.add('dragging');
-
-    //     document.addEventListener('mousemove', onDragging);
-    //     document.addEventListener('touchmove', onDragging);
-    //     document.addEventListener('mouseup', stopDrag);
-    //     document.addEventListener('touchend', stopDrag);
-    // }
-
-    // function onDragging(e) {
-    //     if (!isDragging) return;
-    //     let currentX = e.type.includes('mouse') ? e.pageX : e.touches[0].pageX;
-    //     let currentY = e.type.includes('mouse') ? e.pageY : e.touches[0].pageY;
-
-    //     let deltaX = currentX - startX;
-    //     let deltaY = currentY - startY;
-
-    //     topCard.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-    // }
-
-    // function stopDrag(e) {
-    //     endX = e.type.includes('mouse') ? e.pageX : e.changedTouches[0].pageX;
-    //     endY = e.type.includes('mouse') ? e.pageY : e.changedTouches[0].pageY;
-
-    //     let deltaX = endX - startX;
-    //     let deltaY = endY - startY;
-
-    //     if (Math.abs(deltaX) > 50 || Math.abs(deltaY) > 50) { // Threshold for drag
-    //         flyAway(deltaX, deltaY);
-    //     } else {
-    //         resetDragState();
-    //     }
-    // }
-
-    // function resetDragState() {
-    //     isDragging = false;
-    //     topCard.classList.remove('dragging');
-    //     topCard.style.removeProperty('transform');
-    //     document.removeEventListener('mousemove', onDragging);
-    //     document.removeEventListener('touchmove', onDragging);
-    //     document.removeEventListener('mouseup', stopDrag);
-    //     document.removeEventListener('touchend', stopDrag);
-    // }
-
-function flyAway(deltaX, deltaY, isButtonTriggered = false) {
-    isDragging = false;
-    topCard.classList.remove('dragging');
-    topCard.style.transition = 'transform 0.5s, opacity 0.5s';
-
-    // If the flyaway is triggered by the button, use fixed deltaX and deltaY values
-    if (isButtonTriggered) {
-        deltaX = 1000; // Fixed value for horizontal flyaway
-        deltaY = 0;    // No vertical movement
-    }
-
-    topCard.style.transform = `translate(${deltaX}px, ${deltaY}px)`;
-    topCard.style.opacity = '0';
-
-    setTimeout(() => {
-        resetTopCard();
-    }, 500);
-}
-
-// Adjust the drawNewCard function to pass isButtonTriggered = true
-function drawNewCard() {
-    if (isFirstLoad) {
-        flipCardToShowBack(); // Only flip the card on the first load
-        isFirstLoad = false;
-    } else {
-        flipCardToShowBack();
-        setTimeout(() => {
-            flyAway(0, 0, true); // Trigger flyaway with fixed direction
-        }, 600); // Delay to allow flip animation to complete
-    }
-}
-
-    function resetTopCard() {
-        topCard.remove();
-        topCard.style.removeProperty('transform');
-        topCard.style.removeProperty('opacity');
-        topCard.style.removeProperty('transition');
-        document.getElementById('card-container').appendChild(topCard);
-        flipCardToShowBack(); // Ensure card is flipped to show the back
-        loadNewPrompt(promptElement);
     }
 });
